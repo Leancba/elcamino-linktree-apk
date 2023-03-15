97237401f9b5b87669668c4aee6c8cb671332f29
Leancba <leandro.milia096@gmail.com>
2023-03-15 18:06:23 -0300

    hecho el select

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
+import { useState, useEffect } from 'react';
+import './firebase/config'
+
+import {getFirestore , doc, updateDoc, getDoc} from 'firebase/firestore'
 
 export default function App() {
+  const [Producto, setProducto] = useState('');
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    const querydb = getFirestore();
+    const queryDoc = doc(querydb, 'productos', 'dck0weMkIBJ8qgXGnuPI');
+    getDoc(queryDoc)
+      .then(res => {
+        const data = res.data();
+        const productosArray = Object.entries(data).map(([name, value]) => ({ name, value }));
+        setProducto(productosArray);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  const handleSelectItem = (itemValue, itemIndex) => {
+    setSelectedItem(Producto?.[itemIndex]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Picker
+        selectedValue={selectedItem ? selectedItem.name : null}
+        onValueChange={handleSelectItem}>
+        {Producto && Producto.map((item) => (
+          <Picker.Item key={item.name} label={item.name} value={item.name} />
+        ))}
+      </Picker>
+      <Text>{selectedItem ? selectedItem.value : null}</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
